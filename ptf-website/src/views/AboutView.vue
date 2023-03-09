26c591a0f796e9d4e53b1a8180d75d5e696e7226
bridgetf7 <fbridget0404@gmail.com>
2023-03-09 00:55:32 -0500

    commit

--- ptf-website/src/views/AboutView.vue
+++ ptf-website/src/views/AboutView.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="about">
     <h1>Our Mission:</h1>
+
+    <p>
+  We have all heard the stories before. The terrible experiences that children in foster care must observe, 
+the lack of a strengthened and supporting foundation, the constant back and forth, unable to ever settle, 
+or feel like they were important. When one of our founders heard a story like this from none other than her own 
+parent, she was determined to make a change, whether it be big or small.
+<br>
+  In February of 2021, Pens to Friends was born. Its goal started out as a determined desire to better the 
+  experiences of children in unfortunate situations, even if it's only just to put a smile on their faces.
+<br>
+  With new people coming in and sharing their stories, Pens to Friends has now expanded to spreading light all over the community. The smallest of things can make a day brighter, whether it be a piece of art, a heartfelt story, or even just a few kind words.
+Here at Pens to Friends, our goal is to band the world together. One letter at a time.
+</p>
   </div>
 </template>
 
