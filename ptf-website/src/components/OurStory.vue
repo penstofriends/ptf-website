62e2fae9c628a3c2c767756b451ab5debe0bfb11
bridgetf7 <fbridget0404@gmail.com>
2023-03-12 00:29:40 -0500

    commit

---
+++ ptf-website/src/components/OurStory.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+about us
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- ptf-website/src/views/HomePage.vue
+++ ptf-website/src/views/HomePage.vue
@@ -1,9 +1,12 @@
 <template>
 <h1>welcome home</h1>
+<OurStory />
 </template>
 
 
 <script setup>
+import OurStory from '../components/OurStory.vue';
+
 
 </script>
 
