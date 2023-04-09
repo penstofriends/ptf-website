7af06a7336783329fe87e93af78dd3be48006b67
bridgetf7 <fbridget0404@gmail.com>
2023-04-09 01:27:16 -0400

    Update NavTab.vue

--- ptf-website/src/components/NavTab.vue
+++ ptf-website/src/components/NavTab.vue
@@ -1,53 +1,62 @@
 <template>
-    <div>
-      <nav>
-        <a href="/">Home</a>
-        <a href="/GettingInvolved">Get Involved</a>
-        <a href="/FrequentQuestions">FAQ</a>
-        <a href="/ContactUs">Contact Us</a>
-      </nav>
-    </div>
-  </template>
-  
-  <style scoped>
-  body {
-    margin: 0;
-  }
-  
-  nav {
-    font-family: 'Noto Sans', sans-serif;
-    letter-spacing: 0.1rem;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    text-align: center;
-    background-color: white;
-    padding: 1rem 0rem 1rem 0rem;
-  }
-  
-  nav a {
-    font-size: 1.2rem;
-    color: #646c64;
-    display: flex;
-    justify-content: center;
-    padding: 0 1rem;
-    margin: 0.5rem;
-    width: fit-content;
-    white-space: nowrap;
-    text-decoration: none;
-  }
-  
-  nav a:first-of-type {
-    border: none;
-  }
-  
-  nav a.router-link-exact-active {
-    color: var(--turfGreen);
-  }
-  
-  nav a.router-link-exact-active:hover {
-    background-color: transparent;
-  }
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/GettingInvolved">Get Involved</router-link>
+      <router-link to="/FrequentQuestions">FAQ</router-link>
+      <router-link to="/ContactUs">Contact Us</router-link>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+body {
+  margin: 0;
+}
+
+nav {
+  font-family: 'Noto Sans', sans-serif;
+  letter-spacing: 0.1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  background-color: white;
+  padding: 1rem 0rem 1rem 0rem;
+}
+
+nav a {
+  font-size: 1.2rem;
+  color: #646c64;
+  display: flex;
+  justify-content: center;
+  padding: 0 1rem;
+  margin: 0.5rem;
+  width: fit-content;
+  white-space: nowrap;
+  text-decoration: none;
+}
+
+nav a:first-of-type {
+  border: none;
+}
+
+nav a.router-link-exact-active {
+  color: var(--turfGreen);
+}
+
+nav a.router-link-active {
+  opacity: 60%;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a:hover {
+opacity: 60%;
+  transition: all 1s ease;
+}
 </style>
